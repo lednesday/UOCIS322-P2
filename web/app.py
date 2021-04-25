4af6fd701885048b777d2179c8a323bdfef79dee
Lindsay Marean <lmarean@bensay.org>
2021-04-25 10:26:34 -0700

    it all works on the local machine now

--- web/app.py
+++ web/app.py
@@ -5,6 +5,7 @@ Lindsay Marean's Flask API.
 from flask import Flask, render_template, send_from_directory, abort
 import os
 import os.path
+import sys
 
 
 app = Flask(__name__)
@@ -16,19 +17,21 @@ def home():
     return "UOCIS docker demo!\n"
 
 
-@app.route("/<page>")
+@app.route("/<path:page>")
 def return_page(page):
     # If a page starts with or includes one of the symbols(`~` `//` `..`), return 403 Forbidden
+    # print("page: ", page, file=sys.stderr)
     if '//' in page or '..' in page or '~' in page:
         abort(403)
     # what's the path?
     path, filename = os.path.split(page)
+    # hardcoding pages/ as a sort of DOCROOT
     path = './pages/' + path
-    if not (filename in os.listdir(path)):
+    if not os.path.isfile('./pages/' + page):
         abort(404)
     if (page[-4:] != 'html') and (page[-3:] != 'css'):
         abort(404)
-    return send_from_directory(path, page)  # pathname, filename
+    return send_from_directory(path, filename), 200  # pathname, filename
 
 
 @app.errorhandler(404)
